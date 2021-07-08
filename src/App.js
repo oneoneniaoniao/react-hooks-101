8e138b2baecd7f7a5cf9e6f856a8ff41acdf2ec8
oneoneniaoniao <321marumaru123@gmail.com>
2021-07-08 22:36:00 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = (props) => {
   const [state, setState] = useState(props);
   const {name, price} = state
+
+  //useEffectはレンダリング（return）の後に呼ばれる
+  useEffect(() =>{
+    console.log("This is like componentDidMount or componentDidUpdated")
+  })
+
+  useEffect(() =>{
+    console.log("This is like componentDidMount.")
+  },[])
+
+  useEffect(() =>{
+    console.log("This callback is for name only.")
+  },[name])
+
   return (
     <>
       <p>
-        現在の{name}は、{price}です
+        現在の{name}は、{price}です。
       </p>
       <button onClick={() => setState({...state, price: price + 100})}>+100</button>
       <button onClick={() => setState({...state, price: price - 100})}>-100</button>
