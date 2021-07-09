cb7e3bca738e1765f2accb0aeedc0de41108785b
oneoneniaoniao <321marumaru123@gmail.com>
2021-07-09 14:23:49 +0900

    display events, refactor and implement remove event button

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useState, useReducer} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Event from "./Event"
+import reducer from "../reducers/index"
 
-function App() {
+function App() {          
+  const initialState = []
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const [title, setTitle] = useState("")
+  const [body, setBody] = useState("")
+
+  const addEvent = (e) => {
+    e.preventDefault()
+    dispatch({type:"CREATE_EVENT", title, body})
+    setTitle("")
+    setBody("")
+  }
+
+  console.log({state})
+
   return (
     <div className="container">
       <h4>イベント作成フォーム</h4>
@@ -9,13 +25,13 @@ function App() {
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル </label>
           {/* クリックするとhtmlFor(for)で指定した値をidにもつ要素にフォーカスが当たるようになる */}
-      <input className="form-control" id="formEventTitle"></input>
+      <input className="form-control" id="formEventTitle" value={title} onChange={(e)=>setTitle(e.target.value)}></input>
         </div>
         <div className="form-group">
           <label htmlFor="formEventBody">内容 </label>
-      <textarea className="form-control" id="formEventBody"></textarea>
+      <textarea className="form-control" id="formEventBody" value={body} onChange={e=>setBody(e.target.value)}></textarea>
         </div>
-        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
         <button className="btn btn-danger">全てのイベントを削除する</button>
       </form>
 
@@ -24,7 +40,10 @@ function App() {
         <thead>
           <tr><th>ID</th><th>タイトル</th><th>内容</th><th>削除</th></tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {state.map((event,index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
+      
+        </tbody>
       </table>
     </div>
   );
